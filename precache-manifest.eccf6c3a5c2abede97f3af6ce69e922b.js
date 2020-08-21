49feb3d56d43983f216b3b5642cbaf02dfb53c47
amjedidiah <imunacode@gmail.com>
2020-08-21 09:54:20 +0100

    Updates

---
+++ precache-manifest.eccf6c3a5c2abede97f3af6ce69e922b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e1570bf2bf5c2e0b0050b2938ea406",
+    "url": "/ko-found-me/index.html"
+  },
+  {
+    "revision": "8d5bbb1c78a2d8552b3b",
+    "url": "/ko-found-me/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "9682ac47acc53c85d1aa",
+    "url": "/ko-found-me/static/css/main.255a9523.chunk.css"
+  },
+  {
+    "revision": "8d5bbb1c78a2d8552b3b",
+    "url": "/ko-found-me/static/js/2.bed95b5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ko-found-me/static/js/2.bed95b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9682ac47acc53c85d1aa",
+    "url": "/ko-found-me/static/js/main.9418c0ea.chunk.js"
+  },
+  {
+    "revision": "abe734f1aa1843453579",
+    "url": "/ko-found-me/static/js/runtime-main.2b76c870.js"
+  },
+  {
+    "revision": "5b494077d82a6d543627620f3def5d12",
+    "url": "/ko-found-me/static/media/about.5b494077.svg"
+  },
+  {
+    "revision": "0ba02ead7ffa22e641e65c7e47cd4193",
+    "url": "/ko-found-me/static/media/icon.0ba02ead.png"
+  },
+  {
+    "revision": "31b6d3147308e219284118634fa9d8ae",
+    "url": "/ko-found-me/static/media/isaac.31b6d314.png"
+  },
+  {
+    "revision": "be15f2f518a0a7f506cb548f7a9f4775",
+    "url": "/ko-found-me/static/media/john.be15f2f5.png"
+  },
+  {
+    "revision": "3e65ed9e3255add60004b29cfcef6086",
+    "url": "/ko-found-me/static/media/jumbo.3e65ed9e.svg"
+  },
+  {
+    "revision": "452412e87d687da49a9c6fb85370512f",
+    "url": "/ko-found-me/static/media/theresa.452412e8.png"
+  }
+]);
